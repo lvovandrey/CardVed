0ec51f8ab57f921f07a001b4ef71e2bd578ef453
aa.lvov@i-teco.ru <aa.lvov@i-teco.ru>
2024-03-05 21:32:15 +0300

    Добавлен первый проект Domain

---
+++ Backend/CardVed.Domain/Card.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardVed.Domain
+{
+    public class Card
+    {
+        public required string Name { get; set; }
+    }
+}
