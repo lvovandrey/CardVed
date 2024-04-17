bfbf09caef9a694f390e57ee297cd910cc1f42b3
aa.lvov@i-teco.ru <aa.lvov@i-teco.ru>
2024-04-17 23:01:41 +0300

    Добавил черновик для автомаппинга

---
+++ Backend/App/CardVed.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace CardVed.Application.Common.Mappings
+{
+    public interface IMapWith<T>
+    {
+
+        void CreateMap(Profile profile) =>
+            profile.CreateMap(typeof(T), GetType());
+
+    }
+}
